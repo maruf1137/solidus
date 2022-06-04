2435ca466fdc076ccb14311673939c5dde3cffb3
Mainul Islam <mainul@conversiondev.com>
2022-06-04 15:24:02 +0600

    update

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,19 @@ $('.select_ul li').click(function () {
 	$('.default_option li').html(currentele);
 	$(this).parents('.select_wrap').removeClass('active');
 });
+
+// sidebar
+const sidebar = document.querySelector('.sidebar');
+const btnShow = document.querySelector('.bars');
+const btnClose = document.querySelector('.close-btn');
+
+function showSidebar() {
+	sidebar.classList.add('show-sidebar');
+}
+
+function closeSidebar() {
+	sidebar.classList.remove('show-sidebar');
+}
+
+btnShow.addEventListener('click', showSidebar);
+btnClose.addEventListener('click', closeSidebar);
